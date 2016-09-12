3717d7b74b184a5a99283a69f7e7c584cb9684fe
ssalter2 <sarahsalter@Sarahs-MacBook-Pro.local>
2016-09-11 23:38:19 -0400

    These are the first documents for my project.

--- DataSci_GitHub.R
+++ DataSci_GitHub.R
@@ -4,3 +4,14 @@
 #2) Click GitHub repository button
 #3) Create an R-Markdown Document 
 #4) Go to Tools --> Version Control --> Git
+
+#*************************************************************************************************************************
+
+#1) Go to the terminal 
+#2) Type into terminal the following commands:
+
+    #Last login: Thu Sep  8 15:40:00 on console
+    #Sarahs-MacBook-Pro:~ sarahsalter$ cd ~/Desktop/DataScience/
+    #Sarahs-MacBook-Pro:DataScience sarahsalter$ git remote add origin https://github.com/ssalter2/Data-Science-Project.git
+    #Sarahs-MacBook-Pro:DataScience sarahsalter$ git push -u origin master
+
