6bb2c714adaa03977360adcb6dd98167beba5484
Sarah Salter <sarahsalter@Sarahs-MacBook-Pro.local>
2016-09-11 22:38:28 -0400

    These are the starting files for my course project.

---
+++ DataSci_GitHub.R
@@ -0,0 +1,6 @@
+#Data Science GitHub Notes
+
+#1) Create a New Project --> New Directory --> Empty Project
+#2) Click GitHub repository button
+#3) Create an R-Markdown Document 
+#4) Go to Tools --> Version Control --> Git
